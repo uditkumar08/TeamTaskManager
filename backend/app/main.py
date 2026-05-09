56898fd93c58dcdf9b53178935cc9ad6fe821aee
uditkumar08 <udit2162@gmail.com>
2026-05-09 21:03:39 +0530

    Fix production CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://task-manager-eighti.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
