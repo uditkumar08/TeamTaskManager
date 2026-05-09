d16770d5ae0cc6b9fef7cd9a2a7d80594ee16eb0
uditkumar08 <udit2162@gmail.com>
2026-05-09 22:43:11 +0530

    Updated CORS for new Vercel domain

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://task-manager-eighti.vercel.app"
+        "https://teamttaskmanager.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
