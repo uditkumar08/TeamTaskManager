3836e5ed3b9ee1337626f7343ba099a579cd9633
uditkumar08 <udit2162@gmail.com>
2026-05-09 21:38:50 +0530

    layout fixed

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -5,19 +5,19 @@ function Layout({ children }) {
 
   return (
 
-    <div className="flex">
+    <div className="flex min-h-screen bg-gray-100">
 
       <Sidebar />
 
-      <div className="flex-1 bg-gray-100 min-h-screen">
+      <div className="flex-1 flex flex-col">
 
         <Navbar />
 
-        <div className="p-8">
+        <main className="p-8 flex-1 overflow-y-auto">
 
           {children}
 
-        </div>
+        </main>
 
       </div>
 
